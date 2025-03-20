9db639d28961e0557b46944bacf3f32234f737ab
unknown <sohansaidul978@gmail.coom>
2025-03-20 05:07:51 -0700

    first project

---
+++ java.js
@@ -0,0 +1,33 @@
+// JavaScript Loop
+
+let car = ['BMW', 'Volvo', 'Toyota', 'Ford', 'Tesla', 'Lamburgini'];
+
+// for (let i = 0; i < car.length; i++){
+// console.log(car[i]);
+// }
+
+// let person = {
+//     name: 'Sohan',
+//     age: 30,
+//     country: 'Bangladesh',
+//     job: 'Web developer'
+// };
+
+// for (let i in person) {
+//     let capLetter = i.charAt(0).toUpperCase() + i.slice(1);
+//     // console.log(capLetter + ': ' + person[i]);
+// }
+
+// car.forEach(function(i) {
+    // console.log(i);
+// });
+
+// for (let i of car) {
+//     // console.log(i);
+// }
+
+let num = 0;
+while (num < 10) {
+console.log(num);
+num++;
+}
